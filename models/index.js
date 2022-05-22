ff4453820791f4139f3fd2ca76d4d3b0b2627de0
Christopher Langner <langnerc@icloud.com>
2022-05-22 14:32:28 -0500

    added post api's

--- models/index.js
+++ models/index.js
@@ -1,4 +1,11 @@
-const User = require('./User'); 
-const Post = require('./Post')
+const User = require("./User");
+const Post = require("./Post");
 
-module.exports = {User, Post}; 
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+module.exports = { User, Post };
